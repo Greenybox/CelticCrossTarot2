02e7c18ea865178eb14e244a3a027bdf6ba3d0d8
馮元愷 <greenybox@gmail.com>
2018-08-01 22:45:12 +0800

    Initial Commit

---
+++ CelticCrossTarot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CelticCrossTarot
+//
+//  Created by Greenybox on 2018/8/1.
+//  Copyright © 2018年 Greenybox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
